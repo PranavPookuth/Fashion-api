4deec49e55e1d71d5be074dd506b29ae7267879b
PranavPookuth <pranavpookuth@gmail.com>
2024-11-05 19:30:11 +0530

    first commit

---
+++ fashionapp/models.py
@@ -0,0 +1,74 @@
+from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
+from django.contrib.auth.models import AbstractUser, Permission, Group, PermissionsMixin
+from django.db import models
+
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+class User(AbstractUser):
+    name = models.CharField(max_length=100, null=True, blank=True)
+    email = models.EmailField(unique=True)
+    mobile_number = models.CharField(max_length=15, unique=True,default="123456789")
+    is_verified = models.BooleanField(default=False)
+    otp = models.CharField(max_length=6, null=True, blank=True)
+    otp_secret_key = models.CharField(max_length=32, blank=True, null=True)
+    address = models.TextField(null=True, blank=True)
+    state = models.CharField(max_length=50, null=True, blank=True)
+    city = models.CharField(max_length=50, null=True, blank=True)
+    road_name = models.TextField(default="road name")
+    pincode = models.IntegerField(null=True, blank=True)
+    DOB = models.DateField(null=True, blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['mobile_number', 'name']
+
+    objects = CustomUserManager()
+
+    # Define groups with a unique related_name
+    groups = models.ManyToManyField(
+        Group,
+        related_name='custom_user_set',  # Change this to avoid clashes
+        blank=True
+    )
+
+    # Define user_permissions with a unique related_name
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='custom_user_set',  # Change this to avoid clashes
+        blank=True
+    )
+
+    def __str__(self):
+        return self.email
+
+
+class PasswordResetUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    otp_secret_key = models.CharField(max_length=32, blank=True, null=True)
+    otp_created_at = models.DateTimeField(blank=True, null=True)
+    groups = models.ManyToManyField(Group, related_name='passwordresetuser_set', blank=True, verbose_name=('groups'))
+    user_permissions = models.ManyToManyField(Permission, related_name='passwordresetuser_set', blank=True,
+                                              verbose_name=('user permissions'))
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+
+class products(models.Model):
+    product_name = models.CharField(max_length=100,null=False)
+    Description = models.CharField(max_length=100, null=False)
---
+++ fashionapp/views.py
@@ -0,0 +1,152 @@
+import random
+
+
+from django.contrib.auth import login
+
+from rest_framework.response import Response
+from django.shortcuts import render
+from .models import *
+from .serializers import *
+from rest_framework import generics,status
+from rest_framework.views import APIView
+from django.core.mail import send_mail
+from django.contrib.auth import get_user_model
+
+# Create your views here.
+
+class RegisterView(APIView):
+    permission_classes = []
+    authentication_classes = []
+
+    def post(self, request):
+        email = request.data.get('email')
+        mobile_number = request.data.get('mobile_number')
+
+        try:
+            user = User.objects.get(email=email, mobile_number=mobile_number)
+
+            if user.is_verified:
+                return Response({'error': 'User with this email and mobile number is already verified.'},
+                                status=status.HTTP_400_BAD_REQUEST)
+
+            otp = random.randint(100000, 999999)
+            user.otp = otp
+            user.save()
+
+            send_mail(
+                'OTP Verification',
+                f'Your new OTP is {otp}',
+                'praveencodeedex@gmail.com',
+                [user.email]
+            )
+
+
+            return Response({'message': 'A new OTP has been sent to your email. Please verify your OTP.'},
+                            status=status.HTTP_200_OK)
+
+        except User.DoesNotExist:
+            serializer = RegisterSerializer(data=request.data)
+            if serializer.is_valid():
+                user = serializer.save()
+                otp = random.randint(100000, 999999)
+                user.otp = otp
+                user.save()
+
+                send_mail(
+                    'OTP Verification',
+                    f'Your OTP is {otp}',
+                    'praveencodeedex@gmail.com',
+                    [user.email]
+                )
+
+
+                return Response({'message': 'OTP Sent successfully! Please verify your OTP.'},
+                                status=status.HTTP_201_CREATED)
+
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class OTPVerifyView(APIView):
+    permission_classes = []
+    authentication_classes = []
+
+    def post(self, request):
+        serializer = OTPVerifySerializer(data=request.data)
+        if serializer.is_valid():
+            email = serializer.data['email']
+            otp = serializer.data['otp']
+            try:
+                user = User.objects.get(email=email)
+                if user.otp == otp:
+                    user.is_active = True
+                    user.is_verified = True
+                    user.otp = None
+                    user.save()
+                    return Response({'message': 'Email verified successfully! You can now log in.'},
+                                    status=status.HTTP_200_OK)
+                return Response({'error': 'Invalid OTP'}, status=status.HTTP_400_BAD_REQUEST)
+            except User.DoesNotExist:
+                return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(APIView):
+    permission_classes = []
+    authentication_classes = []
+
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            login(request, user)
+            return Response({'message': 'Logged in successfully!', 'user_id': user.id,'status':True}, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LogoutView(APIView):
+    def post(self,request):
+        response=Response()
+        response.delete_cookie('jwt')
+        response.data={
+            'message':'logout successful',
+            'status':'True'
+        }
+        return response
+
+
+User = get_user_model()
+
+class PasswordResetView(generics.GenericAPIView):
+    serializer_class = PasswordResetSerializer
+
+    def post(self, request):
+        email = request.data.get('email', None)
+        user = User.objects.filter(email=email).first()
+
+        if user:
+            otp = ''.join([str(random.randint(0, 9)) for _ in range(4)])
+
+            user.otp_secret_key = otp
+            user.save()
+
+            email_subject = 'Password Reset OTP'
+            email_body = f'Your OTP for password reset is: {otp}'
+            to_email = [user.email]
+            send_mail(email_subject, email_body, from_email=None, recipient_list=to_email)
+
+
+            return Response({'detail': 'OTP sent successfully.','status':True}, status=status.HTTP_200_OK)
+        else:
+            return Response({'detail': 'User not found.','status':False}, status=status.HTTP_404_NOT_FOUND)
+
+
+class Usercreateview(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class Userdetails(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+
+
+
